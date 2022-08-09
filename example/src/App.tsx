0ad2fbf0d63f0df4d0322a2277fbd9147341347f
Raí Siqueira <rai93siqueira@gmail.com>
2022-08-09 20:49:39 -0300

    init :tada:
    Signed-off-by: Raí Siqueira <rai93siqueira@gmail.com>

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import { lib } from 'lib';
+const Main = () => {
+  return (
+    <div>
+      <h1>Hello, awesome {lib()}! This is your playground.</h1>
+    </div>
+  );
+};
+
+export default Main;
